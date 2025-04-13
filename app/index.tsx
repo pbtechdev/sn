5534818e391c461775aec5c218226c9ac1cc5dc5
Phanindra Babu Angadala <phanindrababu.angadala@changiairport.com>
2025-04-13 17:18:26 +0530

    test commit

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text } from "@/components/ui/text"
 export default function Index() {
   return (
     <Box className="bg-primary-500 p-5">
-      <Text className="text-typography-0">This is the Box</Text>
+      <Text className="text-typography-0">Test commit 2</Text>
     </Box>
   );
 }
